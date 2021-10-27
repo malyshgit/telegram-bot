99c3ae1db46c2e2d0428cfd1a2916013923c997e
malyshgit <vm.malysh@gmail.com>
2021-10-27 21:59:33 +0500

    fix jdk version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,30 @@
+import com.pengrad.telegrambot.TelegramBot;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
 
-    public static void main(String[] args) {
-        if(args.length > 0){
-
-        }
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create();
+        server.bind(new InetSocketAddress(Integer.parseInt(System.getenv("PORT"))), 0);
+        server.createContext("/", new MainHandler());
+        server.setExecutor(null);
+        server.start();
     }
 
+    private static class MainHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String request = new BufferedReader(new InputStreamReader(exchange.getRequestBody(), StandardCharsets.UTF_8)).readLine();
 
+            System.out.println("Запрос: "+request);
+        }
+    }
 }
