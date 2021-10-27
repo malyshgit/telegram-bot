e138584c5f9a2f1f9b2f2893739752a07adb3d74
malyshgit <vm.malysh@gmail.com>
2021-10-27 22:54:33 +0500

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,13 @@
 import com.pengrad.telegrambot.BotUtils;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ForceReply;
+import com.pengrad.telegrambot.model.request.ParseMode;
+import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.request.SetWebhook;
+import com.pengrad.telegrambot.response.SendResponse;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -54,6 +59,18 @@ public class Main {
             }catch (IOException e) {
                 e.printStackTrace();
             }
+
+            SendMessage sendMessage = new SendMessage(update.message().chat().id(), update.message().text())
+                    .parseMode(ParseMode.HTML)
+                    .disableWebPagePreview(true)
+                    .disableNotification(true)
+                    .replyToMessageId(1)
+                    .replyMarkup(new ForceReply());
+
+// sync
+            SendResponse sendResponse = bot.execute(sendMessage);
+            boolean ok = sendResponse.isOk();
+            Message message = sendResponse.message();
         }
     }
 }
